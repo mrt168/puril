7eb9c130566b6e0bce5400c706d8f4568553cfae
mrt168 <kurafuto16@gmail.com>
2020-01-31 17:00:56 +0900

    merge

--- webroot/js/front/all.js
+++ webroot/js/front/all.js
@@ -763,3 +763,17 @@ $(window).on('load scroll resize',function(){
 		});
 	}
 });
+
+//ハンバーガーメニュー
+$(function() {
+  $(document).on("click", ".datsumou-header-inner__navToggle", function() {
+    $(".datsumou-header-inner__navToggle").toggleClass("active");
+    $(".datsumou-header-inner__globalmenusp").toggleClass("active");
+  });
+
+  $(".datsumou-header-inner__globalmenusp__link").on("click", function() {
+    if (window.innerWidth <= 768) {
+      $(".datsumou-header-inner__navToggle").click();
+    }
+  });
+});
